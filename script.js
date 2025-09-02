6a324924b74a23f30f2bb223f7a41fbd4f019a5c
Mohit Pareek <mohitpareek88515@gmail.com>
2025-09-02 12:27:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+      strings: ['Frontend Engineer','Backend Engineer','C++ Developer','SQL Expert'],
+      typeSpeed: 100,
+      backspeed: 100,
+      loop: true 
+    });
+
+
+
